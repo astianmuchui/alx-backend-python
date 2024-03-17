02bcc88b2ce410b2c05e3d9eb2174f5538a1773d
astianmuchui <astianmuchui@gmail.com>
2024-03-17 19:43:11 +0300

    annotated math floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Annotated function floor that takes a float n as argument
+and returns an integer"""
+
+
+def floor(n: float) -> int:
+    """ Function floor that takes a float n as argument
+    and returns an integer"""
+    return int(n)
