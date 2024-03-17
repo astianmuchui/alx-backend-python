325818a6d3b7df0891892c3b121d4c1c8e91bf99
astianmuchui <astianmuchui@gmail.com>
2024-03-17 19:40:26 +0300

    annotated concatenation function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Annotated function concat that takes a
+string str1 and a string str2 as arguments"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Function concat that takes a string str1
+    and a string str2 as arguments"""
+    return str1 + str2
