2d78e8b564fdd2d643d8493cc7353d9ed554b52f
astianmuchui <astianmuchui@gmail.com>
2024-03-17 19:47:43 +0300

    annotated to str function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Annotated function that takes in
+a float n as argument and returns the
+string representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """ Function that takes in a float n as argument
+    and returns the string representation of the float"""
+    return str(n)
