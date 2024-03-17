971a2420b66d41edb4c523248b737e293992a28a
astianmuchui <astianmuchui@gmail.com>
2024-03-17 19:38:34 +0300

    module documentation and pycodestyle validation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""Annotated function add that takes
+a float a and a float b as arguments"""
+
 
 def add(a: float, b: float) -> float:
+    """ Function add that takes a float a and a float b as arguments"""
     return (a + b)
