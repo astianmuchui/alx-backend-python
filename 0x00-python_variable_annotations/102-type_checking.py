cfc9afbb42abb9b2d2386b083ad9ad52a6266316
astianmuchui <astianmuchui@gmail.com>
2024-03-17 20:40:27 +0300

    typechecking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -17,8 +17,8 @@ def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
     return zoomed_in
 
 
-array = (12, 72, 91)  # Use parentheses to create a tuple
+array: Tuple = (12, 72, 91)
 
-zoom_2x = zoom_array(array)
+zoom_2x: Tuple = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x: Tuple = zoom_array(array, 3)
