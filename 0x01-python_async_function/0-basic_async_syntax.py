e4a84d53cc977ff5b3ab7bc2b98d79b852523715
astianmuchui <astianmuchui@gmail.com>
2024-03-19 10:50:00 +0300

    Init files

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+asynchronous coroutine
+that takes in an integer argument (max_delay, with a default value of 10)
+named wait_random that waits for a random delay between 0 and max_delay
+(inclusive and float value) seconds and eventually returns it.
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    """Asynchronous coroutine that takes in an integer argument"""
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,3 @@
+## Introduction to Asynchronous Programming in Python
+
+This directory contains my python async IO tasks
