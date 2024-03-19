a0b590676c6b1216aa1ed5773d384da9be0a875c
astianmuchui <astianmuchui@gmail.com>
2024-03-19 11:13:33 +0300

    Concurrent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 
+"""
+Import wait_random from 0-basic_async_syntax
+and write a function wait_n that takes in 2 int arguments
+(max_delay: int and n: int) and returns a list of n floats.
+
+"""
+
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(max_delay: int, n: int) -> List[float]:
+    """Asynchronous coroutine that takes in 2 int arguments"""
+    list_of_delays: List[float] = [await wait_random
+                                   (max_delay) for _ in range(n)]
+    return sorted(list_of_delays)
