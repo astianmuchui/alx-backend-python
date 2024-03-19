3b7d1b7fb0a1699f9a0b5497144ad2d1c3c6a9f5
astianmuchui <astianmuchui@gmail.com>
2024-03-19 11:01:21 +0300

    Annotated variables

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,6 +12,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> int:
     """Asynchronous coroutine that takes in an integer argument"""
-    random_delay = random.uniform(0, max_delay)
+    random_delay: int = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
