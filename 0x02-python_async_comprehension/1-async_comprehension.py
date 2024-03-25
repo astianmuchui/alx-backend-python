864c55a0ac8783c66867879c7fdf93225a994890
astianmuchui <astianmuchui@gmail.com>
2024-03-25 15:56:48 +0300

    Attempted the 1st task

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,12 +8,12 @@ collects the 10 random numbers and returns them
 """
 import asyncio
 import random
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     coroutine async_comprehension
     that takes no arguments
