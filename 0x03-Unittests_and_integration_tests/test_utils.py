f31644e18b8045d7be118179e930df158c191928
astianmuchui <astianmuchui@gmail.com>
2024-04-25 01:43:17 +0300

    Added test access nested map exception method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,12 +26,11 @@ class TestAccessNestedMap(TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand(
-        {
-            ({}, ("a",),),
-            ({"a": 1}, ("a", "b")),
-        }
-    )
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+
     def test_access_nested_map_exception(self, nested_map, path):
         """
         Tests for keyerror exception
